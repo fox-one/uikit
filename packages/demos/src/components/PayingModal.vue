c67dfc44a005b8618cfdb63051f6039924ef9b60
Lyric <5h3ll3x@gmail.com>
2021-01-14 19:21:19 +0900

    mod: update text

--- packages/demos/src/components/PayingModal.vue
+++ packages/demos/src/components/PayingModal.vue
@@ -2,7 +2,7 @@
   <v-row>
     <v-col>
       <v-btn outlined rounded block color="primary" @click="handlePaying">
-        支付
+        Pay
       </v-btn>
     </v-col>
     <f-paying-modal :show="show" :text="text" @cancel="handleCancel" />
@@ -16,7 +16,7 @@ import { Component, Vue } from "vue-property-decorator";
 class PayingModal extends Vue {
   show = true;
 
-  text = "正在检查支付结果";
+  text = "Check result";
 
   handlePaying() {
     this.show = true;
--- packages/demos/src/components/TIps.vue
+++ packages/demos/src/components/TIps.vue
@@ -2,30 +2,29 @@
   <v-row>
     <v-col cols="12">
       <f-tip type="warning">
-        警告：如果不这样做，你会遭到伤害
+        WARNING: UIKit has no brain, use your own!
         <br />
-        错误码：1234
+        Code: 1234
       </f-tip>
     </v-col>
     <v-col cols="12">
       <f-tip type="error">
-        失败原因：吧啦吧啦吧啦
+        ERROR: Failed to restart your brain, eat more!
         <br />
-        错误码：1234
+        Code: 0001
       </f-tip>
     </v-col>
     <v-col cols="12">
       <f-tip type="info">
-        这是一段重要的提示
+        This is an important notice.
         <br />
-        如果真的读了就会得到奖励。
+        Read it and you'll get rewards.
       </f-tip>
     </v-col>
     <v-col>
       <f-tip type="text">
-        一般般的提示。
-        <br />
-        就是这个样子
+        Some boring text here, make no sense. <br />
+        Just like your life.
       </f-tip>
     </v-col>
   </v-row>
